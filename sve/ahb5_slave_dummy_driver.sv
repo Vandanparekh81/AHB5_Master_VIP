82453525e8f3170b007ffa5263e42167c38cf66e
Vandanparekh81 <vandanparekh4587@gmail.com>
2025-04-25 19:09:48 +0530

    Changes added in non -reusable components

--- sve/ahb5_slave_dummy_driver.sv
+++ sve/ahb5_slave_dummy_driver.sv
@@ -1,8 +1,9 @@
+import ahb5_pkg::*;
 class ahb5_slave_dummy_driver;
   logic [31:0] Hrdata;
   logic Hresp;
   logic Hready;
-  logic [31:0] mem [*];
+  logic [63:0] mem [*];
   ahb5_transaction trans;
   virtual ahb5_interface intf;
 
@@ -21,6 +22,7 @@ class ahb5_slave_dummy_driver;
       if(intf.Hwrite == 1 && intf.Hready == 1 && intf.Htrans != 0) begin
         mem[intf.Haddr] = intf.Hwdata;
         intf.Hresp = 0;
+        $display("656565556565565 TIme = %0t | memory = %0h | memory without address = %p ", $time, mem[intf.Haddr], mem);
       end
 
       if(intf.Hwrite == 0 && intf.Hready == 0 && intf.Htrans != 0) begin
--- sve/ahb5_test.sv
+++ sve/ahb5_test.sv
@@ -1,11 +1,11 @@
-`include "../sv/ahb5_environment.sv"
+//`include "../sv/ahb5_environment.sv"
+import ahb5_pkg::*;
 program ahb5_test(ahb5_interface intf);
+  testcase_t testcase;
+  
   ahb5_environment env;
-
-
-
   initial begin
-    env = new(intf);
+    env = new(intf, 5, 5, WRITE_TEST);    
     env.test_run();
   end
 endprogram
